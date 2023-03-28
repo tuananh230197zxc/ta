f4796a5af2cdb77f823194b3d4d6ba06d2d9c6ec
tuananh230197zxc <88013981+tuananh230197zxc@users.noreply.github.com>
2023-03-28 21:53:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 const nameGirl = 'Em';
-const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng 20-10';
 const titleCard = 'Tặng người ấy';
 const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
